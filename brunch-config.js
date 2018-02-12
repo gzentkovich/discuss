ee70d0fd8cbd88210a657a5b7e15dfb064d56e69
G. Zentkovich <gzentkovich@gmail.com>
2018-02-11 19:34:16 -1000

    bulk commit, lazy

--- brunch-config.js
+++ brunch-config.js
@@ -2,8 +2,10 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
-
+      joinTo: {
+        "js/app.js": /^(web\/static\/js)/,
+        "js/materialize.min.js": /^(web\/static\/js)/
+      }
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
       // joinTo: {
--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,11 +1,31 @@
 defmodule Discuss.TopicController do
     use Discuss.Web, :controller
+
+    alias Discuss.Topic
     
     def index(conn, _params) do
         render conn, "index.html"
     end
 
     def new(conn, _params) do
-        render conn, "new.html"
+        changeset = Topic.changeset(%Topic{}, %{})
+        render conn, "new.html", changeset: changeset
+    end
+
+    def create(conn, %{"topic" => topic}) do
+        changeset = Topic.changeset(%Topic{}, topic)
+
+        case Repo.insert(changeset) do
+            {:ok, post} -> IO.inspect(post)
+            {:error, changeset} -> IO.inspect(changeset)
+        end
+    end
+
+    def edit(conn, _params) do
+
+    end
+
+    def update(conn, _params) do
+
     end
 end

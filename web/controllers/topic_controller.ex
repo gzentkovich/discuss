5bbad54b166474857584719680d44f10f7eb7588
G. Zentkovich <gzentkovich@gmail.com>
2018-02-11 16:22:01 -1000

    add new topic controller

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,11 @@
+defmodule Discuss.TopicController do
+    use Discuss.Web, :controller
+    
+    def index(conn, _params) do
+        render conn, "index.html"
+    end
+
+    def new(conn, _params) do
+        render conn, "new.html"
+    end
+end

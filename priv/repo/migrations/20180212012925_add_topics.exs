3dc5d504e379b2063b3c5f4b4235ec14f8bd7588
G. Zentkovich <gzentkovich@gmail.com>
2018-02-11 15:52:30 -1000

    created topics table in database

--- priv/repo/migrations/20180212012925_add_topics.exs
+++ priv/repo/migrations/20180212012925_add_topics.exs
@@ -2,6 +2,10 @@ defmodule Discuss.Repo.Migrations.AddTopics do
   use Ecto.Migration
 
   def change do
-
+    # create a new table called "topics"
+    create table(:topics) do
+      # add a column named "title" with a type of "string"
+      add :title, :string
+    end
   end
 end

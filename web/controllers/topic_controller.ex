09a7fb5fce9a8a3b8da0542bbb94395c8470943c
Gregory Zentkovich <gzentkovich@gmail.com>
2018-02-13 16:03:32 -1000

    formatting

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -25,6 +25,7 @@ defmodule Discuss.TopicController do
                 conn
                 |> put_flash(:info, "Topic Created")
                 |> redirect(to: topic_path(conn, :index))
+
             {:error, changeset} ->
                 conn
                 |> put_flash(:warning, "Text field can not be blank")

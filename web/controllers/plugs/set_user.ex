cb093789138ef9b04498bafb0e835de80ec6dc8d
Gregory Zentkovich <gzentkovich@gmail.com>
2018-02-14 14:40:25 -1000

    removed extra lines

--- web/controllers/plugs/set_user.ex
+++ web/controllers/plugs/set_user.ex
@@ -6,13 +6,11 @@ defmodule Discuss.Plugs.SetUser do
     alias Discuss.User
 
     def init(_params) do
-        # no setup required, yet...
     end
 
-    # this _params is from the init function above, if any.
     def call(conn, _params) do
       user_id = get_session(conn, :user_id)
-      
+
       cond do
           user = user_id && Repo.get(User, user_id) ->
             assign(conn, :user, user)

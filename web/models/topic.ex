2043a04e92f874f4c506ae4d9a3d5a79a4893ab5
G. Zentkovich <gzentkovich@gmail.com>
2018-02-21 22:07:34 -1000

    add has_many association with comment model

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -4,6 +4,7 @@ defmodule Discuss.Topic do
     schema "topics" do
         field :title, :string
         belongs_to :user, Discuss.User
+        has_many :comments, Discuss.Comment
     end
 
     @doc false

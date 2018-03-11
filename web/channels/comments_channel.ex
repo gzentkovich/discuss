06ace90c6de1eab892ba6e4654134dd69c9fbe0c
G. Zentkovich <gzentkovich@gmail.com>
2018-03-10 14:46:54 -1000

    fix auth

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -2,13 +2,10 @@ defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
   def join(name, _params, socket) do
-    IO.puts("||||||||||||||||||||||||||||||||||||||||||")
-    IO.puts(name)
-    IO.puts("||||||||||||||||||||||||||||||||||||||||||")
     {:ok, %{hey: "there"}, socket}
   end
 
-  def handle_in() do
-
+  def handle_in(name, message, socket) do
+    {:reply, :ok, socket}
   end
 end

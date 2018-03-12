48b3cb3807d9e8a685e31c48b3449541b867158e
G. Zentkovich <gzentkovich@gmail.com>
2018-03-11 14:29:59 -1000

    add comments to topic

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -4,12 +4,16 @@ defmodule Discuss.CommentsChannel do
 
   def join("comments:" <> topic_id, _params, socket) do
     topic_id = String.to_integer(topic_id)
-    topic = Repo.get(Topic, topic_id)
+    #topic = Repo.get(Topic, topic_id)
+    topic = Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(:comments)
 
-    {:ok, %{}, assign(socket, :topic, topic)}
+    # return this
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
 
-  def handle_in(name, %{"content" => content}, socket) do
+  def handle_in(_name, %{"content" => content}, socket) do
     topic = socket.assigns.topic
 
     changeset = topic
@@ -18,6 +22,9 @@ defmodule Discuss.CommentsChannel do
 
     case Repo.insert(changeset) do
       {:ok, comment} ->
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new",
+          %{comment: comment}
+        )
         {:reply, :ok, socket}
       {:error, _reason} ->
         {:reply, {:error, %{errors: changeset}}, socket}
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -6,16 +6,42 @@ socket.connect()
 const createSocket = (topicId) => {
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => {
+      renderComments(resp.comments);
+    })
     .receive("error", resp => { console.log("Unable to join", resp) })
+
+  channel.on(`comments:${topicId}:new`, renderComment);
 
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value;
 
     channel.push('comments:add', { content: content });
   });
+};
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return commentTemplate(comment);
+  });
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
 }
 
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
+
+  document.querySelector('.collection').innerHTML += renderedComment;
+  document.querySelector('.materialize-textarea').value = "";
+}
+
+function commentTemplate(comment) {
+    return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+    `;
+}
 /*
 add our createsokcet function on to the window scope, now it can be called from everywhere
 */
